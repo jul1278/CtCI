af5086ce65b2076c9e45741a8f3ce5fd8e523400
jul1278 <jul1278@gmail.com>
2018-04-11 23:12:41 +1000

    added depth first search

--- Graphs.cpp
+++ Graphs.cpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 struct Node {
     // constructing the graph might take a little bit longer but algorithms on the graph will be faster
@@ -16,6 +17,10 @@ struct Graph {
     // cant resize this
     std::vector<Node> nodes; 
 }; 
+
+// NOTE: remember that a graph might be stored like the following
+typedef uint16_t NodeKey; 
+std::unordered_map<NodeKey, std::tuple<std::string, std::unordered_set<NodeKey>>> alternateGraph; 
 
 //------------------------------------------------------------------------------------
 // Name: FromAdjacencyMatrix 
@@ -46,6 +51,9 @@ void FromAdjacencyMatrix(Graph& graph, uint8_t* adjacencyMatrix, std::vector<std
 
 // Chapter 4 
 // Breadth First Search
+// O(n) memory (i think? worst case we store a pointer to each node...)
+// O(n) complexity 
+//
 bool BreadthFirstSearch(const Graph& graph, const Node* start = nullptr, const Node* find = nullptr) {
     std::deque<const Node*> queue; 
     std::unordered_set<const Node*> visited; 
@@ -57,31 +65,30 @@ bool BreadthFirstSearch(const Graph& graph, const Node* start = nullptr, const N
         if (result != graph.nodes.end()) {    
             queue.push_back(start); 
             visited.insert(start); 
+        } else {
+            return false; 
         }
 
     } else {
 
         visited.insert(&graph.nodes[0]);
-        // TODO: 
-        // reserve space in the queue
         queue.push_back(&graph.nodes[0]); 
     }
 
     while (!queue.empty()) {
         auto top = queue.front(); 
         queue.pop_front();
-
         std::cout << top->name << " "; 
+
         for (auto child : top->children) {
             if (visited.find(child) == visited.end()) {
                 visited.insert(child);  
 
-                if (child == find) {
+                if (find && (child == find)) {
                     std::cout << child->name << "\n";
                     return true; 
                 }
 
-                // visit 
                 queue.push_back(child); 
             }
         }
@@ -90,6 +97,42 @@ bool BreadthFirstSearch(const Graph& graph, const Node* start = nullptr, const N
     std::cout << "\n";
 
     return false; 
+}
+
+// Depth first search
+// possible use is in simulations for example where 
+//
+void DepthFirstSearch(const Graph& graph) {
+    if (graph.nodes.size() == 0) { return; }
+    
+    std::unordered_set<const Node*> visitedNodes; 
+    std::vector<std::tuple<const Node*, unsigned int>> stack; 
+    stack.push_back(&graph.nodes[0]); 
+
+    while (!stack.empty()) {
+        auto& top = stack.back(); 
+        auto& visited = std::get<1>(top); 
+        auto currentNode = std::get<0>(top); 
+
+        if (visited == 0) {
+            std::cout << currentNode->name << " "; 
+        }
+
+        if (visited < currentNode->children.size()) {
+            for (auto node : currentNode->children) {
+                visited++; 
+                if (visitedNodes.find(node) == visitedNodes.end()) {
+                    visitedNodes.insert(node); 
+                    stack.push_back(std::tuple<const Node*, unsigned int>(node, 0)); 
+                    break; 
+                }
+            }
+        } else {
+            stack.pop_back(); 
+        }
+    }
+
+    std::cout << "\n"; 
 }
 
 // 4.1
@@ -116,7 +159,7 @@ int main() {
         {0, 0, 0, 0, 0, 0}  // 5 F
     }; 
 
-    std::vector<std::string> values = { "0","1","2","3","4","5" };
+    std::vector<std::string> values = { "0", "1", "2", "3", "4", "5" };
 
     Graph graph;
     FromAdjacencyMatrix(graph, (uint8_t*)adjacencyMatrix, values, 6); 
@@ -124,7 +167,9 @@ int main() {
     BreadthFirstSearch(graph);
 
     auto result = BreadthFirstSearch(graph, &graph.nodes[2], &graph.nodes[4]);
-    std::cout << "\n" << result << "\n";
+    std::cout << result << "\n";
+
+    DepthFirstSearch(graph);
 
     return 0; 
 }
